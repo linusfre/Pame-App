3cdb3788bc88827e226b37b6de8d00a94a3b0dcc
linusfre <linusfrenzer@gmail.com>
2022-12-27 20:28:43 +0100

    few changes

---
+++ party.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Party erstellen</title>
+</head>
+<body>
+    <div class="secondary_container party_create_container">
+        <h1>Party erstellen</h1>
+        <form action="" method="POST">
+            <label for="name">Name</label>
+            <input type="name" id="name" name="name" spellcheck="false" placeholder="Gebe den Namen der Party ein" required>
+            
+            <label for="description"
+</body>
+</html>
